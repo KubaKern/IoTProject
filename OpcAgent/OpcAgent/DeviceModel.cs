f44cec0bbb80323d3612622421e59002bdbdb692
KubaKern <kerniaks@gmail.com>
2023-05-04 20:07:27 +0200

    initial commit, opcagent, device simulator, device model

---
+++ OpcAgent/OpcAgent/DeviceModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Opc.UaFx;
+using Opc.UaFx.Client;
+namespace OpcAgent
+{
+    internal class DeviceModel
+    {
+        OpcReadNode productionStatus;
+        OpcReadNode productionRate;
+        OpcReadNode workOrderId;
+        OpcReadNode temperature;
+        OpcReadNode goodCount;
+        OpcReadNode badCount;
+        OpcReadNode deviceError;
+    }
+}
